9bf8f8ff70e8d93c436dc4dbaa3b6231fa4f9aec
oteplov <oteplov@softserveinc.com>
2022-01-22 15:59:57 +0200

    HW#4 Task#1

---
+++ src/main/java/com/softserveinc/task01/CoffeeMachineV1.java
@@ -0,0 +1,19 @@
+package com.softserveinc.task01;
+
+import com.softserveinc.task01.AbstractCoffeeMachine;
+
+public class CoffeeMachineV1 extends AbstractCoffeeMachine {
+    public CoffeeMachineV1(int coffeeBeanStorageCapacity, int waterReservoirCapacity, int wasteCoffeeBeanCapacity) {
+        super(coffeeBeanStorageCapacity, waterReservoirCapacity, wasteCoffeeBeanCapacity);
+    }
+
+    @Override
+    public boolean makeEspresso() {
+        return makeCoffee(22,30);
+    }
+
+    @Override
+    public boolean makeAmericano() {
+        return makeCoffee(22,100);
+   }
+}
